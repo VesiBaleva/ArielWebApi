189bef211b189f32da121f6852dec2d5f66ef3b7
Lina Chalakova <chalakovalina@gmail.com>
2013-08-14 16:59:44 +0300

    saved solution added IRepsitory

--- ArielWebRecipe/ArielWebRecipe.Repositories/IRepository.cs
+++ ArielWebRecipe/ArielWebRecipe.Repositories/IRepository.cs
@@ -6,7 +6,16 @@ using System.Threading.Tasks;
 
 namespace ArielWebRecipe.Repositories
 {
-    public class IRepository
+    public interface IRepository<T> where T : class
     {
+        IQueryable<T> All();
+
+        T Get(int id);
+
+        void Add(T item);
+
+        void Delete(int id);
+
+        void Update(int id, T item);
     }
 }

70dc18bad5310d80c84accb57717eac8f4df8f9b
Lina Chalakova <chalakovalina@gmail.com>
2013-08-14 22:20:33 +0300

    Changed RecipeController

--- ArielWebRecipe/ArielWebRecipe.WebApi/Models/RecipeInfo.cs
+++ ArielWebRecipe/ArielWebRecipe.WebApi/Models/RecipeInfo.cs
@@ -1,20 +1,37 @@
-﻿using System;
+﻿using ArielWebRecipe.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Web;
 
 namespace ArielWebRecipe.WebApi.Models
 {
     public class RecipeInfo
     {
+        public int Id { get; set; }
+
         public string Title { get; set; }
 
         public string PictureLink { get; set; }
 
         public string AuthorName { get; set; }
 
-        public int Likes { get; set; }
-
         public int PreparationTime { get; set; }
+
+        public static Expression<Func<Recipe, RecipeInfo>> FromRecipe
+        {
+            get
+            {
+                return x => new RecipeInfo 
+                { 
+                    Id = x.Id, 
+                    Title = x.Title, 
+                    AuthorName=x.Author.UserName,
+                    PictureLink=x.PictureLink, 
+                    PreparationTime=x.PreparationSteps.Select(y=>y.PreparationTime).Sum()
+                };
+            }
+        }
     }
 }

400def3971ad1e057263a47c2c558e40ebd5ede3
Lina Chalakova <chalakovalina@gmail.com>
2013-08-14 18:48:12 +0300

    added DbRecipeRepository and DbPreparationStepRepository

---
+++ ArielWebRecipe/ArielWebRecipe.Repositories/DbPreparationStepRepository.cs
@@ -0,0 +1,78 @@
+﻿using ArielWebRecipe.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArielWebRecipe.Repositories
+{
+    public class DbPreparationStepRepository:IRepository<PreparationStep>
+    {
+        private DbContext dbContext;
+        private DbSet<PreparationStep> entitySet;
+
+        public DbPreparationStepRepository(DbContext dbContext)
+        {
+            this.dbContext = dbContext;
+            this.entitySet = this.dbContext.Set<PreparationStep>();
+        }
+
+        public PreparationStep Get(int id)
+        {
+            return this.entitySet.Where(s => s.Id == id).FirstOrDefault();
+        }
+
+        public IQueryable<PreparationStep> All()
+        {
+            return this.entitySet;
+        }
+
+        public PreparationStep Add(PreparationStep step)
+        {
+            this.entitySet.Add(step);
+            this.dbContext.SaveChanges();
+
+            return step;
+        }
+
+        public PreparationStep Update(int id, PreparationStep step)
+        {
+            PreparationStep queryStep = this.entitySet.Where(u => u.Id == id).FirstOrDefault();
+            if (step.Order != null)
+            {
+                queryStep.Order = step.Order;
+            }
+            if (step.PictureLink != null)
+            {
+                queryStep.PictureLink = step.PictureLink;
+            }
+            if (step.PreparationTime != null)
+            {
+                queryStep.PreparationTime = step.PreparationTime;
+            }
+            if (step.Recipe != null)
+            {
+                queryStep.Recipe = step.Recipe;
+            }
+
+            this.dbContext.SaveChanges();
+
+            return queryStep;
+        }
+
+        public void Delete(int id)
+        {
+            PreparationStep step = this.Get(id);
+            this.entitySet.Remove(step);
+            this.dbContext.SaveChanges();
+        }
+
+        public void Delete(PreparationStep step)
+        {
+            this.entitySet.Remove(step);
+            this.dbContext.SaveChanges();
+        }
+    }
+}
---
+++ ArielWebRecipe/ArielWebRecipe.Repositories/DbRecipeRepository.cs
@@ -0,0 +1,82 @@
+﻿using ArielWebRecipe.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArielWebRecipe.Repositories
+{
+    public class DbRecipeRepository : IRepository<Recipe>
+    {
+        private DbContext dbContext;
+        private DbSet<Recipe> entitySet;
+
+        public DbRecipeRepository(DbContext dbContext)
+        {
+            this.dbContext = dbContext;
+            this.entitySet = this.dbContext.Set<Recipe>();
+        }
+
+        public Recipe Get(int id)
+        {
+            return this.entitySet.Where(r => r.Id == id).FirstOrDefault();
+        }
+
+        public IQueryable<Recipe> All()
+        {
+            return this.entitySet;
+        }
+
+        public Recipe Add(Recipe recipe)
+        {
+            this.entitySet.Add(recipe);
+            this.dbContext.SaveChanges();
+
+            return recipe;
+        }
+
+        public Recipe Update(int id, Recipe recipe)
+        {
+            Recipe queryRecipe = this.entitySet.Where(u => u.Id == id).FirstOrDefault();
+            if (recipe.Title != null)
+            {
+                queryRecipe.Title = recipe.Title;
+            }
+            if (recipe.PictureLink != null)
+            {
+                queryRecipe.PictureLink = recipe.PictureLink;
+            }
+            if (recipe.Author != null)
+            {
+                queryRecipe.Author = recipe.Author;
+            }
+            if (recipe.Comments != null)
+            {
+                queryRecipe.Comments = recipe.Comments;
+            }
+            if (recipe.PreparationSteps!=null)
+            {
+                queryRecipe.PreparationSteps = recipe.PreparationSteps;
+            }
+
+            this.dbContext.SaveChanges();
+
+            return queryRecipe;
+        }
+
+        public void Delete(int id)
+        {
+            Recipe recipe = this.Get(id);
+            this.entitySet.Remove(recipe);
+            this.dbContext.SaveChanges();
+        }
+
+        public void Delete(Recipe recipe)
+        {
+            this.entitySet.Remove(recipe);
+            this.dbContext.SaveChanges();
+        }
+    }
+}

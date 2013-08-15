211384c565d790b935af27f6f835cd7278c5f105
nim-ohtar <angel.n.stoyanov@gmail.com>
2013-08-14 20:39:47 -0700

    Demo request made

--- ArielWebRecipe/ArielWebRecipe.WebClient/Scripts/myController.js
+++ ArielWebRecipe/ArielWebRecipe.WebClient/Scripts/myController.js
@@ -7,22 +7,43 @@ var controllers = (function () {
 
     var updateTimer = null;
 
-    var rootUrl = "http://arialwebapirecipe.apphb.com/api";
+    var rootUrl = "http://arialwebapirecipe.apphb.com/api/";
     var Controller = Class.create({
         init: function () {
             this.persister = persisters.get(rootUrl);
         },
         loadUI: function (selector) {
 
-            this.loadBaseUI(selector);
+            //this.loadBaseUI(selector);
 
             this.attachUIEventHandlers(selector);
+        },
+
+        loadGameUI: function (selector) {
+            var self = this;
+            var gameUIHtml = "<button id='get'>Get<button>"
+				
+            $(selector).html(gameUIHtml);
         },
 
         attachUIEventHandlers: function (selector) {
             var wrapper = $(selector);
             var self = this;
 
+            //Basic request
+            wrapper.on("click", "#newRequest", function () {
+                console.log("Even on newRequest");
+
+                self.persister.user.test(function (data) {
+                    wrapper.find("#requestContent").html(JSON.stringify(data));
+                },
+
+				function (err) {
+				    alert(JSON.stringify(err));
+				});
+
+                return false;
+            });
             
         },
     });
@@ -35,5 +56,5 @@ var controllers = (function () {
 
 $(function () {
     var controller = controllers.get();
-    controller.loadUI("#content");
+    controller.loadUI("#wrapper");
 });

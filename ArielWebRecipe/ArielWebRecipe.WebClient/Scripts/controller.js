df444c328a23a5dc557798fa8b540346902c75f8
Vesi Baleva <bveselina@gmail.com>
2013-08-14 22:27:11 -0700

    UI commit

--- ArielWebRecipe/ArielWebRecipe.WebClient/Scripts/controller.js
+++ ArielWebRecipe/ArielWebRecipe.WebClient/Scripts/controller.js
@@ -1,1 +1,91 @@
-﻿
+﻿/// <reference path="class.js" />
+/// <reference path="jquery-2.0.2.js" />
+/// <reference path="ui.js" />
+
+var controllers = (function() {
+    var rootUrl = "http://arialwebapirecipe.apphb.com/api/";
+
+    var updateTimer = null;
+
+    var Controller = Class.create({
+        init: function () {
+            //set on persister
+         //   this.persister = persisters.get(rootUrl);
+        },
+
+        loadUI: function (selector) {
+            //set on persister
+            //if (this.persister.isUserLoggedIn()) {
+                this.loadMainUI(selector);
+           //}
+           //else {
+           //    this.loadLoginFormUI(selector);
+           //}
+           //this.attachUIEventHandlers(selector);
+        },
+
+        loadLoginFormUI: function (selector) {
+            var loginFormHtml = ui.loginForm()
+            $(selector).html(loginFormHtml);
+        },
+
+        loadMainUI: function (selector) {
+            var self = this;
+            var mainUIHtml =
+				ui.mainUI("Vesi");                      //this.persister.nickname());
+            $(selector).html(mainUIHtml);
+
+            this.updateUI(selector);
+
+            updateTimer = setInterval(function () {
+                self.updateUI(selector);
+            }, 80000);
+        },
+
+        updateUI: function (selector) {
+            var recipeExample = {
+                id: "",
+                title: "Title",
+                pictureLink: "img/images.jpg"
+            }
+
+            var recipe_list = [];
+
+            for (var i = 0; i < 9; i++) {
+                recipeExample.id = i + 1;
+                recipeExample.title += i;
+                recipe_list.push(recipeExample);
+            }
+
+            var list = ui.recipesList(recipe_list);
+
+            $(selector + " #recipes-list")
+                .html(list);
+            var userOperationUIHtml =
+                ui.userOperationUI("Vesi");
+            $(selector + " #user-operation").html(userOperationUIHtml);
+
+            //this.persister.recipesAll(function (recipes) {
+                
+
+            //    var list = ui.recipesList(recipe_list);
+            //    $(selector + " #recipes-list")
+			//		.html(recipe_list);
+            //    var userOperationUIHtml =
+            //        ui.userOperationUI(this.persister.nickname());
+            //    $(selector + " #user-operation").html(userOperationUIHtml);
+            //});            
+        }
+    });
+    return {
+        get: function () {
+            return new Controller();
+        }
+    }
+
+}());
+
+$(function () {
+    var controller = controllers.get();
+    controller.loadUI("#container");
+})
--- ArielWebRecipe/ArielWebRecipe.WebClient/Scripts/ui.js
+++ ArielWebRecipe/ArielWebRecipe.WebClient/Scripts/ui.js
@@ -1,1 +1,87 @@
-﻿
+﻿var ui = (function () {
+
+    function buildLoginForm() {
+        var html = 
+            '<div id="login-form-holder">' +
+				'<form>' +
+					'<div id="login-form">' +
+						'<label for="tb-login-username">Username: </label>' +
+						'<input type="text" id="tb-login-username"><br />' +
+						'<label for="tb-login-password">Password: </label>' +
+						'<input type="text" id="tb-login-password"><br />' +
+						'<button id="btn-login" class="button">Login</button>' +
+					'</div>' +
+					'<div id="register-form" style="display: none">' +
+						'<label for="tb-register-username">Username: </label>' +
+						'<input type="text" id="tb-register-username"><br />' +
+						'<label for="tb-register-nickname">Nickname: </label>' +
+						'<input type="text" id="tb-register-nickname"><br />' +
+						'<label for="tb-register-password">Password: </label>' +
+						'<input type="text" id="tb-register-password"><br />' +
+						'<button id="btn-register" class="button">Register</button>' +
+					'</div>' +
+					'<a href="#" id="btn-show-login" class="button selected">Login</a>' +
+					'<a href="#" id="btn-show-register" class="button">Register</a>' +
+				'</form>' +
+				'<div id="error-messages"></div>' +
+            '</div>';
+        return html;
+    }
+
+    function buildMainUI(nickname) {
+        var html = 
+		    '<span id="user-nickname">Welcome, ' +
+				nickname +
+		    '</span>' +
+		    '<button id="btn-logout">Logout</button><br/>' +
+            '<h2>A list of the recipes</h2>' +
+            '<div class="row">' +                
+                '<div class="span9">' +
+                '<div id="recipes-list"></div>' +
+                '</div>' +
+                '<div class="span3">' +
+                '<div id="user-operation"></div>' +
+                '</div>' +
+            '</div>';
+        return html;
+    }
+
+    function buildRecipesList(recipes) {
+        var list = '<div class="row-fluid">';
+        
+        for (var i = 0; i < recipes.length; i++) {
+
+            var recipe = recipes[i];
+            list +=
+				'<div class="span4" data-recipe-id="' + recipe.id + '">' +
+					'<a href="#" >' + $("<div> /").html(recipe.title).text() + '</a>' +
+                    '<img src="' + $("<div />").html(recipe.pictureLink).text() + '" class="img-polaroid" />' +
+				'</div>';
+        }
+        list += "</div>";
+        return list;
+    }
+
+    function buildUserOperationUI(nickname) {
+        var html =
+		    '<form class="form-search">' +
+                  '<div>' +
+                      '<input type="text" class="input-medium search-query" />' +
+                      '<button type="submit" class="btn">Search</button>' +
+                  '</div>' +
+            '</form>' +
+            '<div>' +
+                '<button class="btn btn-large btn-block" type="button">Create recipe</button>' +
+            '</div>';
+        return html;
+    }
+
+
+    return {
+        loginForm:buildLoginForm,
+        mainUI:buildMainUI,
+        recipesList:buildRecipesList,
+        userOperationUI:buildUserOperationUI
+    }
+
+}());

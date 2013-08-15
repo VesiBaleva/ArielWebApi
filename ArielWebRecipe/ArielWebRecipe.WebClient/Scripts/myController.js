0dcac0bdb5ea016005e39299cb02d62567c165ab
nim-ohtar <angel.n.stoyanov@gmail.com>
2013-08-14 19:56:48 -0700

    Client communication basics

---
+++ ArielWebRecipe/ArielWebRecipe.WebClient/Scripts/myController.js
@@ -0,0 +1,39 @@
+﻿/// <reference path="class.js" />
+/// <reference path="persister.js" />
+/// <reference path="jquery-2.0.2.js" />
+/// <reference path="ui.js" />
+
+var controllers = (function () {
+
+    var updateTimer = null;
+
+    var rootUrl = "http://arialwebapirecipe.apphb.com/api";
+    var Controller = Class.create({
+        init: function () {
+            this.persister = persisters.get(rootUrl);
+        },
+        loadUI: function (selector) {
+
+            this.loadBaseUI(selector);
+
+            this.attachUIEventHandlers(selector);
+        },
+
+        attachUIEventHandlers: function (selector) {
+            var wrapper = $(selector);
+            var self = this;
+
+            
+        },
+    });
+    return {
+        get: function () {
+            return new Controller();
+        }
+    }
+}());
+
+$(function () {
+    var controller = controllers.get();
+    controller.loadUI("#content");
+});
